ea149c0f094e1a08bc75f2e6f40b020f6ac80640
Marc N <git@marcnitzsche.de>
2024-05-26 03:29:05 +0200

    Remove unnecessary code

--- src/bot/commands/create.ts
+++ src/bot/commands/create.ts
@@ -5,15 +5,8 @@ import { type ICommand } from './types';
 
 export class CreateCommand implements ICommand {
   public execute = async (ctx: Context<Update>): Promise<void> => {
-    ctx.session ??= {
-      currentMethod: 'create',
-      currentStep: 1,
-      createData: { title: null, when: null, untilWhen: null, location: null },
-    };
-
     ctx.session.currentMethod = 'create';
     ctx.session.currentStep = 1;
-    ctx.session.createData = { title: null, when: null, untilWhen: null, location: null };
 
     await ctx.reply(`Glad you want to add an experience 🤗
 
